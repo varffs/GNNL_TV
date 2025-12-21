d3e4a1ebbda89f167d0f41be0a429d7955690392
varffs <patrickbest@patrickbest.com>
2025-12-21 00:57:33 +0000

    set base url for GH pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gnnl.tv',
+  // site: 'https://gnnl.tv',
+  base: '/GNNL_TV', // your repo name
   // Configure your site settings here
 });
