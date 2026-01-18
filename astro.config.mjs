fe7a47037f727d3635a1060830893c5cc0132488
varffs <patrickbest@patrickbest.com>
2026-01-18 20:43:10 +0000

    update base
    correct?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://gnnl.tv',
-  base: '/GNNL_TV', // your repo name
-  // Configure your site settings here
 });
