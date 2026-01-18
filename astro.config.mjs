6ff624b5df83babcff570b7bd681d8a758daff96
varffs <patrickbest@patrickbest.com>
2026-01-18 21:03:00 +0000

    out link external links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://gnnl.tv',
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+    ]
+  }
 });
